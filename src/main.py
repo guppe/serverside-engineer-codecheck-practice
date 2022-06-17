6dff05bc09c44b3cbe8dbfc706e78847412ea55a
guppe <mst39639toi8@gmail.com>
2022-06-17 16:50:41 +0900

    データは整理した

--- src/main.py
+++ src/main.py
@@ -1,20 +1,33 @@
 import csv
 import sys
+#import pandas as pd
+import io
 
 def main():
     pathname = sys.argv[1]
-    #with open(file=pathname) as csvfile:
-        #reader = csv.DictReader(csvfile, lineterminator="\n")
     
     with open(pathname, "r") as f:
         reader = csv.reader(f)
         header = next(reader)
-        A = []
+        csvdata = {}
+        num = {}
         for row in reader:
-            A.append(row)
+            key = row[1]
+            if (key in csvdata) == False:
+                csvdata[key] = row[2]
+            elif (key in csvdata) == True:
+                csvdata[key] = int(csvdata[key]) + int(row[2])
+                if (key in num) == False:
+                    num[key] = 1
+                num[key] += 1                
+        #print(csvdata)
+        #print(num)
         
-        #for i in range(len(row)):
-            
+        for i in num:
+            csvdata[i] = str(int(int(csvdata[i]) / int(num[i])))
+        print(csvdata)
+
+          
     
 if __name__ == "__main__":
     main()
